2b10ffdb66c49d553ee641a1b29dfab45ca6db23
jim8489 <130927896+jim8489@users.noreply.github.com>
2025-07-18 11:13:00 +0600

    Update settings.py

--- ShareApp/ShareApp/settings.py
+++ ShareApp/ShareApp/settings.py
@@ -83,14 +83,11 @@ WSGI_APPLICATION = 'ShareApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'file_share',
-        'USER': 'root',
-        'PASSWORD': '8489659Ma',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
